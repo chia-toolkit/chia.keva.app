a3707542f5609a919c14accc241b7c87168c711c
kawww <39939137+kawww@users.noreply.github.com>
2021-05-02 07:12:32 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $resource = fopen('node.log', 'r');
 $arr = array();
 $totalass = array();
 
-echo "<table><tr height=50><td width=160>Chia Nodes</td><td width=50 align=center>PORT</font></td><td width=100 align=center>BLOCK</td></tr>";
+echo "<table><tr height=50><td width=160>Chia Nodes</td><td width=50 align=center>PORT</font></td><td width=100 align=center>BLOCK</td></tr><tr><td>5.189.201.93</td><td align=center><font color=\"8CEA00\">8444</font></td><td  align=center style=\"color:#FFD306;\">[LOCAL*]</td></tr>";
 
 if($a>$now_time)
 
@@ -110,7 +110,6 @@ if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds)){
 echo file_get_contents("good.log");
 
 
-
 echo "</table>";
 
 
@@ -119,10 +118,19 @@ echo "</table>";
            echo "<br>Last Update: ".date("Y-m-d H:i:s",$now_time);
 
 		   ?>
+   <br><br>
+
+introducer-apne.chia.net:8444 <br>
+introducer-apse.chia.net:8444 <br><br>
+introducer-or.chia.net:8444 <br>
+introducer-va.chia.net:8444 <br>
+introducer-eu.chia.net:8444 <br>
 
 		   <br>
 
 <table><tr height=50><td width=150>Links</td><td width=230 align=center></td></tr>
+
+
 
 <tr><td>chia.keva.app</td><td align=left><font color="8CEA00">github.com/chia-toolkit</font></td></tr>
 <tr><td>kevacoin code</td><td align=left><font color="8CEA00">62108412</font></td></tr>
@@ -132,11 +140,17 @@ echo "</table>";
 <tr><td>Chia Explorer</td><td align=left><font color="8CEA00">chiaexplorer.com</font></td></tr>
 <tr><td>Chia Explorer</td><td align=left><font color="8CEA00">chiastatus.com</font></td></tr>
 <tr><td>Chia Netspace</td><td align=left><font color="8CEA00">chianetspace.com</font></td></tr>
+<tr><td>Chia Calculator</td><td align=left><font color="8CEA00">chiacalculator.com</font></td></tr>
 
 <tr><td>-</td><td align=left><font color="8CEA00"></font></td></tr>
 
 <tr><td>Chia Blog</td><td align=left><font color="8CEA00">chiadecentral.com</font></td></tr>
 <tr><td>Chia Blog</td><td align=left><font color="8CEA00">thechiafarmer.com</font></td></tr>
+
+<tr><td>-</td><td align=left><font color="8CEA00"></font></td></tr>
+
+<tr><td>Chia BBS</td><td align=left><font color="8CEA00">reddit.com/r/chia</font></td></tr>
+<tr><td>Chia BBS</td><td align=left><font color="8CEA00">chiaforum.com</font></td></tr>
 
 
 
